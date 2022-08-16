0164c21b9848cb2630bb5d6b458591d2ab559781
Jovin Joju <55528193+jovinjoju1999@users.noreply.github.com>
2022-08-16 13:51:00 +0530

    Time: 3 ms (46.91%), Space: 5.9 MB (99.78%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -1,22 +1,10 @@
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-        int k=-1;
-        for(int i=0;i<word.size();i++)
+       
+        
         {
-            if(word[i]==ch)
-            {
-                k=i;
-                break;
-            }
-        }
-        if(k==-1)
-        {
-            return word;
-        }
-        else
-        {
-            reverse(word.begin(),word.begin()+k+1);
+            reverse(word.begin(),word.begin()+word.find(ch)+1);
             return word;
         }
         
