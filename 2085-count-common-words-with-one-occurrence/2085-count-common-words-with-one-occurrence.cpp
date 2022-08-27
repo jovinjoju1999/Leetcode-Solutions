a0965c12028336a871c8d261709d0d2c3d7ae9b5
Jovin Joju <55528193+jovinjoju1999@users.noreply.github.com>
2022-08-27 17:50:35 +0530

    Time: 79 ms (25.45%), Space: 20.2 MB (20.87%) - LeetHub

---
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countWords(vector<string>& words1, vector<string>& words2) {
+      unordered_map<string,int> mp1;  
+      unordered_map<string,int> mp2; 
+        int count=0;
+        for(auto i:words1)
+        {
+            mp1[i]++;
+        }
+        for(auto i:words2)
+        {
+            mp2[i]++;
+        }
+        for(auto i:words1)
+        {
+            if(mp1[i]==1 && mp2[i]==1)
+            {
+                count++;
+            }
+        }
+        
+        return count;
+        
+        
+    }
+};
